e1981f9b2145abde37bc9fd54bdd7ebf97fab049
Orges Mihaj <orges.mihaj@hotmail.com>
2019-04-28 04:29:23 +0300

    Contact page: Social component; Form;

---
+++ src/components/Social.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="c-social">
+            <ul className="c-social__list">
+                <li className="c-social__item">
+                    <a href="#" className="c-social__link social__link--facebook">Fb.</a>
+                </li>
+                <li className="c-social__item">
+                    <a href="#" className="c-social__link social__link--instagram">Ins.</a>
+                </li>
+                <li className="c-social__item">
+                    <a href="#" className="c-social__link social__link--linkedin">Lin.</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Social;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
+import Social from '../components/Social';
 
 const Contact = () => {
+
+    const formData = {
+        'name': React.useRef(),
+        'email': React.useRef(),
+        'message': React.useRef()
+    }
+
+    const sendData = () => {
+        alert(formData.name.current.value + " ("+ formData.email.current.value +")")
+    }
+
     return (
-        <p>Contact</p>
+        <div className="c-contact">
+
+			<div className="o-row">
+				<form className="o-form" action="">
+
+		    		<div className="o-form__header">
+		    			<h3 className="o-form__title">Contact me</h3>
+						<p className="o-form__subtitle">Send me a message and we'll respond as soon as possible.</p>
+		    		</div>
+
+					<ul className="o-form__list">
+						<li className="o-form__item">
+							<input type="text" ref={formData.name} className="o-form__control" placeholder="your name"/>
+						</li>
+
+						<li className="o-form__item">
+							<input type="email" ref={formData.email} className="o-form__control" placeholder="your email" />
+						</li>
+
+						<li className="o-form__item">
+							<textarea name="message" ref={formData.message} cols="20" rows="5" className="o-form__control o-form__textarea" placeholder="What's up?"></textarea>
+						</li>
+
+						<li className="o-form__item">
+							<input type="submit" className="o-form__button" onClick={sendData} value="send" />
+						</li>
+					</ul>
+		        </form>
+			</div>
+
+			<div className="o-row">
+                <Social />
+			</div>
+
+		</div>
     )
 }
 
